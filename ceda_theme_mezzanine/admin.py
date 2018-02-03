415c5b7b3a94d6967132a7ca3d7617a92ae80438
Matt Pritchard <matt.pritchard@stfc.ac.uk>
2018-02-03 09:01:50 +0000

    re-ordered fields in portfolioitemadmin. fixes #13

--- ceda_theme_mezzanine/admin.py
+++ ceda_theme_mezzanine/admin.py
@@ -4,11 +4,14 @@ from mezzanine.core.admin import TabularDynamicInlineAdmin
 from mezzanine.utils.admin import SingletonAdmin
 from mezzanine.pages.admin import PageAdmin
 
+from copy import deepcopy
+
 from models import (SiteSectionPage,
                     Portfolio, PortfolioItem, PortfolioItemImage,
                     Person,PortfolioItemPerson,
                     PortfolioItemCategory, IconBox, HomePage, Slide)
-
+from django.forms import TextInput, Textarea
+from django.db import models
 
 admin.site.register(SiteSectionPage, PageAdmin)
 
@@ -22,8 +25,14 @@ class PortfolioItemImageInline(TabularDynamicInlineAdmin):
     model = PortfolioItemImage
     max_num = 3
 
+# Specify order for fields from custom model
+portfolioitem_fieldsets = deepcopy(PageAdmin.fieldsets)
+for field in ("icon", "subtitle", "intro", "content", "href", "button_text", "categories"):
+    portfolioitem_fieldsets[0][1]["fields"].insert(-2, field)
+
 class PortfolioItemAdmin(PageAdmin):
     inlines = (PortfolioItemImageInline,PortfolioItemPersonInline)
+    fieldsets = portfolioitem_fieldsets
 
 admin.site.register(Portfolio, PageAdmin)
 admin.site.register(PortfolioItem, PortfolioItemAdmin)
